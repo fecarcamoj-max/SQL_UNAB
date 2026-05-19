1ff61ced25e098a29a09a43b673042ae54c1837f
ingfelipeacj <felipecarcamo@upla.cl>
2026-05-19 19:51:10 -0400

    Add

--- SQL_CLASE_19052026.sql
+++ SQL_CLASE_19052026.sql
@@ -1,6 +1,5 @@
 use clase_sql;
 
-SELECT * FROM id_persona_venta;
 SELECT * FROM inventario; # Maestra Productos.
 SELECT * FROM persona; # Maestra Clientes
 SELECT * FROM ventas; # Ventas
@@ -22,5 +21,24 @@ GROUP BY i.nombre_producto
 ORDER BY SUM(v.cantidad) DESC
 LIMIT 1
 ;
+SELECT * FROM ventas where id_producto = 2;
+SELECT * FROM inventario;
+# Mostrar el producto que menos se vendió y mostrar 
+# quienes compraron (nombre): ########## Pendiente por Terminar.
+SELECT i.nombre_producto, SUM(v.cantidad), GROUP_CONCAT(p.nombre) 
+FROM ventas v
+INNER JOIN inventario as i ON v.id_producto = i.id_producto
+INNER JOIN persona as p ON v.id_persona = p.id_persona
+GROUP BY i.nombre_producto
+ORDER BY SUM(v.cantidad) DESC;
+
+# Mismo ejercicio anterior pero por categoría:
+SELECT i.categoria, SUM(v.cantidad), GROUP_CONCAT(p.nombre) 
+FROM ventas v
+INNER JOIN inventario as i ON v.id_producto = i.id_producto
+INNER JOIN persona as p ON v.id_persona = p.id_persona
+GROUP BY i.categoria
+ORDER BY SUM(v.cantidad) DESC;
+
 
 ### Crear respaldos de tablas en otras bases de datos.
